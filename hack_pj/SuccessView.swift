1fe90a63d9edad6de2c3ad0c30635e8ec904c6f9
hoit1011 <parksm071011@gmail.com>
2024-01-10 15:54:51 +0900

    createView

---
+++ hack_pj/SuccessView.swift
@@ -0,0 +1,85 @@
+//
+//  SuccessView.swift
+//  hack_pj
+//
+//  Created by 박성민 on 1/10/24.
+//
+
+import SwiftUI
+
+struct SuccessView: View {
+    var image: UIImage?
+    var title: String
+    var selectedTags: Set<String> = []
+    var body: some View {
+        VStack{
+            Text("등록 완료")
+                .navigationBarBackButtonHidden()
+            Spacer()
+            Text("등록이 완료되었습니다.")
+                .bold()
+                .font(.system(size: 22))
+            ZStack {
+                Rectangle()
+                    .fill(Color.gray.opacity(0.3))
+                    .frame(width: 150, height: 200)
+                image.map {
+                    Image(uiImage: $0)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width:150, height: 200)
+                        .clipped()
+                }
+            }
+            
+            Text("\(title)")
+                .bold()
+                .font(.system(size: 22))
+            HStack{
+                ForEach(Array(selectedTags), id: \.self) { tag in
+                    Text("\(tag)")
+                        .padding(EdgeInsets(top: 10, leading: 15, bottom: 10, trailing: 15))
+                        .background(Color.pink.opacity(0.2))
+                        .foregroundColor(Color.pink)
+                        .cornerRadius(30)
+                }
+            }
+            Spacer()
+            HStack{
+                Button{
+                    
+                }label: {
+                    Text("돌아가기")
+                        .bold()
+                        .padding(EdgeInsets(top: 15, leading: 50, bottom: 15, trailing: 50))
+                        .foregroundStyle(.orange)
+                        .background(RoundedRectangle(cornerRadius: 5).fill(Color.white))
+                        .overlay(RoundedRectangle(cornerRadius: 5).stroke(Color.orange))
+                }
+                Button{
+                    
+                }label: {
+                    Text("회차 쓰기")
+                        .bold()
+                        .padding(EdgeInsets(top: 15, leading: 50, bottom: 15, trailing: 50))
+                        .foregroundStyle(.white)
+                        .background(RoundedRectangle(cornerRadius: 5).fill(Color.orange))
+                        .overlay(RoundedRectangle(cornerRadius: 5).stroke(Color.orange))
+                }
+            }
+            .padding()
+        }
+    }
+}
+
+#if DEBUG
+struct SuccessView_Previews: PreviewProvider {
+    static var previews: some View {
+        SuccessView(
+            image: UIImage(named: "sampleImage"),
+            title: "Sample Title",
+            selectedTags: ["Tag1", "Tag2"]
+        )
+    }
+}
+#endif
--- hack_pj/createView.swift
+++ hack_pj/createView.swift
@@ -4,41 +4,168 @@ import PhotosUI
 struct CreateView: View {
     @State private var image: UIImage?
     @State private var selectedItems: [PhotosPickerItem] = []
-
+    @State private var title: String = ""
+    @State private var introduce: String = ""
+    let topallTags = ["판타지","SF","로맨스","호러","추리"]
+    let bottomallTags = ["미스테리","코미디","스릴러","기타"]
+    @State private var selectTag: Set<String> = []
+    @State private var showingAlert = false
+    @State private var alertMessage = ""
+    @State private var issucces = false
     var body: some View {
-        VStack{
-            Text("작품 등록")
-            image.map{
-                Image(uiImage: $0)
-                    .resizable()
-                    .scaledToFit()
-            }
-
-            PhotosPicker(
-                selection: $selectedItems,
-                matching: .images
-            ) {
-                Text("이미지 선택")
-            }
-            .onChange(of: selectedItems) { oldValue, newValue in
-                guard let item = selectedItems.first else {
-                    return
-                }
-                item.loadTransferable(type: Data.self) { result in
-                    switch result {
-                    case .success(let data):
-                        if let data = data {
-                            self.image = UIImage(data: data)
-                        } else {
-                            print("Data is nil")
-                        }
-                    case .failure(let failure):
-                        fatalError("\(failure)")
+        NavigationView{
+            VStack{
+                Text("작품 등록")
+                ZStack {
+                    Rectangle()
+                        .fill(Color.gray.opacity(0.3))
+                        .frame(width: 90, height: 130)
+                    image.map {
+                        Image(uiImage: $0)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width:90, height: 130)
+                            .clipped()
                     }
                 }
+                .padding([.top,.bottom],10)
+                PhotosPicker(
+                    selection: $selectedItems,
+                    matching: .images
+                ) {
+                    Text("작품의 표지를 선택해주세요")
+                }
+                .onChange(of: selectedItems) { oldValue, newValue in
+                    guard let item = selectedItems.first else {
+                        return
+                    }
+                    item.loadTransferable(type: Data.self) { result in
+                        switch result {
+                        case .success(let data):
+                            if let data = data {
+                                self.image = UIImage(data: data)
+                            } else {
+                                print("Data is nil")
+                            }
+                        case .failure(let failure):
+                            fatalError("\(failure)")
+                        }
+                    }
+                }
+                HStack{
+                    Text("작품명")
+                        .bold()
+                        .padding([.leading])
+                    Spacer()
+                }.padding([.top],10)
+                ZStack{
+                    Rectangle()
+                        .fill(Color.white)
+                        .frame(width:300,height: 20)
+                        .padding()
+                        .background(RoundedRectangle(cornerRadius: 10).fill(Color.white))
+                        .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.gray))
+                    TextField("작품명을 입력해주세요",text: $title)
+                        .padding([.leading],30)
+                        .foregroundColor(.black)
+                }
+                HStack{
+                    Text("작품 소개")
+                        .bold()
+                        .padding()
+                    Spacer()
+                }
+                ZStack{
+                    Rectangle()
+                        .fill(Color.white)
+                        .frame(width: 300,height: 100)
+                        .padding()
+                        .background(RoundedRectangle(cornerRadius: 10).fill(Color.white))
+                        .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.gray))
+                    TextField("작품 소개를 해주세요",text: $introduce)
+                        .padding(EdgeInsets(top: 0, leading: 30, bottom: 90, trailing: 0))
+                }
+                HStack{
+                    Text("태그 선택")
+                        .bold()
+                        .padding()
+                    Spacer()
+                }
+                HStack {
+                    ForEach(topallTags, id: \.self) { tag in
+                        Button(action: {
+                            if selectTag.contains(tag) {
+                                selectTag.remove(tag)
+                            } else {
+                                selectTag.insert(tag)
+                            }
+                        }) {
+                            Text("\(tag)")
+                                .padding(EdgeInsets(top: 10, leading: 15, bottom: 10, trailing: 15))
+                                .background(selectTag.contains(tag) ? Color.orange : Color.pink.opacity(0.2))
+                                .foregroundColor(selectTag.contains(tag) ? Color.white : Color.pink)
+                                .cornerRadius(30)
+                        }
+                    }
+                }
+                HStack {
+                    ForEach(bottomallTags, id: \.self) { tag in
+                        Button(action: {
+                            if selectTag.contains(tag) {
+                                selectTag.remove(tag)
+                            } else {
+                                selectTag.insert(tag)
+                            }
+                        }) {
+                            Text("\(tag)")
+                                .padding(EdgeInsets(top: 10, leading: 15, bottom: 10, trailing: 15))
+                                .background(selectTag.contains(tag) ? Color.orange : Color.pink.opacity(0.2))
+                                .foregroundColor(selectTag.contains(tag) ? Color.white : Color.pink)
+                                .cornerRadius(30)
+                        }
+                    }
+                }
+                Button{
+                    if image == nil {
+                        alertMessage = "이미지를 선택해주세요."
+                        showingAlert = true
+                    } else if title.isEmpty {
+                        alertMessage = "작품명을 입력해주세요."
+                        showingAlert = true
+                    } else if introduce.isEmpty {
+                        alertMessage = "작품 소개를 작성해주세요."
+                        showingAlert = true
+                    } else if selectTag.isEmpty {
+                        alertMessage = "태그를 선택해주세요."
+                        showingAlert = true
+                    } else{
+                        print("success")
+                        issucces = true
+                    }
+                    
+                }label: {
+                    Text("작품 등록")
+                        .padding(EdgeInsets(top: 15, leading: 140, bottom: 15, trailing: 140))
+                        .foregroundStyle(Color.black)
+                        .background(Color.gray.opacity(0.3))
+                        .cornerRadius(10)
+                }
+                .alert(isPresented: $showingAlert){
+                    Alert(title: Text("오류"),message:Text(alertMessage),dismissButton: .default(Text("확인")))
+                }
+                .padding([.top],30)
+                if(issucces){
+                    NavigationLink(
+                        destination: SuccessView(image: image, title: title, selectedTags: selectTag),
+                        isActive: $issucces
+                    )
+                    {
+                        EmptyView()
+                    }
+                    Spacer()
+                }
             }
-
-            Spacer()
+            .padding()
         }
     }
 }
